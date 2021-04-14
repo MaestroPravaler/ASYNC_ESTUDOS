373c96c2a5a68eaf4a047730f8c2d75f3a65cfe1
Robson-Maestro <robson_maestro@alumni.usp.br>
2021-04-13 22:13:42 -0300

    Initial Commit

---
+++ bin/assincromismo.dart
@@ -0,0 +1,5 @@
+import 'package:assincromismo/assincromismo.dart' as assincromismo;
+
+void main(List<String> arguments) {
+  print('Hello world: ${assincromismo.calculate()}!');
+}
---
+++ test/assincromismo_test.dart
@@ -0,0 +1,8 @@
+import 'package:assincromismo/assincromismo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
